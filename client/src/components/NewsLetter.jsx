530ee00cd7ef1635ac71c95668ad88d19c084d0a
LindtAna <anastasiialindt@gmail.com>
2025-10-17 20:36:31 +0200

    NewsLetter Section implemented

---
+++ client/src/components/NewsLetter.jsx
@@ -0,0 +1,25 @@
+const NewsLetter = () => {
+    return (
+        <div className="flex flex-col items-center justify-center text-center
+        space-y-2 mt-20 pb-16">
+            <h1 className="md:text-4xl text-2xl font-semibold">Verpasse kein Angebot!</h1>
+            <p className="md:text-lg text-dark-green/70 pb-10">
+                Abonniere unseren Newsletter, um die neuesten Angebote, Neuheiten und exklusive Rabatte zu erhalten.
+            </p>
+            <form className="flex items-center justify-between 
+            max-w-2xl w-full md:h-13 h-12">
+                <input
+                    className="border border-primary-dull/20 rounded-md h-full border-r-0 
+                    outline-none w-full rounded-r-none px-3 text-black placeholder:text-primary"
+                    type="text"
+                    placeholder="E-Mail-Adresse eingeben"
+                    required
+                />
+                <button type="submit" className="md:px-12 px-8 h-full text-white bg-primary hover:bg-dark-green transition-all cursor-pointer rounded-md rounded-l-none">
+                    Abonnieren
+                </button>
+            </form>
+        </div>
+    )
+}
+export default NewsLetter
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from '../components/Hero'
 import Categories from '../components/Categories'
 import Angebote from '../components/Angebote'
 import Features from '../components/Features'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
 <Categories/>
 <Angebote />
 <Features />
+<NewsLetter />
     </div>
   )
 }
