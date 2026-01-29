7da3ae9bb90287e3da3887b39d090c552e84f043
LindtAna <anastasiialindt@gmail.com>
2026-01-29 16:57:59 +0100

    Behebung von Problemen mit Hinzuef von Adresse

--- server/controllers/userAddressController.js
+++ server/controllers/userAddressController.js
@@ -2,12 +2,32 @@ import UserAddress from "../models/UserAddress.js";
 
 // Add User Address : /api/address/add
 export const addAddress = async (req, res) => {
-     try{
-        const {userId, addressData} = req.body;
-        await UserAddress.create({...addressData, userId})
+    try {
+        const userId = req.user.id;
+        const {
+            firstName,
+            lastName,
+            phone,
+            street,
+            postcode,
+            city,
+            country
+        } = req.body;
+
+        await UserAddress.create({
+            userId,
+            firstName,
+            lastName,
+            phone,
+            street,
+            postcode,
+            city,
+            country
+        });
+
         return res
             .status(201)
-            .json({ success: true, message: "Lieferadresse hinzugefügt"});
+            .json({ success: true, message: "Lieferadresse hinzugefügt" });
 
     } catch (error) {
         console.log(error.stack);
@@ -19,13 +39,14 @@ export const addAddress = async (req, res) => {
 // get address: api/address/get
 
 export const getAddress = async (req, res) => {
-     try{
-        const{ userId} = req.body
-        const addresses = await UserAddress.find({userId})
-    
+    try {
+
+        const userId = req.user.id;
+        const addresses = await UserAddress.find({ userId })
+
         return res
             .status(200)
-            .json({ success: true, addresses});
+            .json({ success: true, addresses });
 
     } catch (error) {
         console.log(error.stack);
