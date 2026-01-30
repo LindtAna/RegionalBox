cae0198111e44df196a3ab0c75e8b7c3af882e4c
LindtAna <anastasiialindt@gmail.com>
2026-01-30 20:44:40 +0100

    Seller-/demoSeller-Auth korrigiert

--- server/middleware/authSeller.js
+++ server/middleware/authSeller.js
@@ -2,26 +2,30 @@ import jwt from "jsonwebtoken";
 
 const authSeller = async (req, res, next) => {
        const { sellerToken } = req.cookies;
-    if (!sellerToken)
-        return res.json({ success: false, message: 'Nicht autorisiert' });
+    if (!sellerToken) {
+        return res.status(401).json({
+            success: false,
+            message: "Nicht autorisiert"
+        });
+    }
 
     try {
-        const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
+        const decoded = jwt.verify(sellerToken, process.env.JWT_SECRET)
 
-        const isSeller = (tokenDecode.email === process.env.SELLER_EMAIL) ||
-        (tokenDecode.email === process.env.SELLER_DEMO_EMAIL);
+        const isSeller =
+            decoded.email === process.env.SELLER_EMAIL ||
+            decoded.email === process.env.SELLER_DEMO_EMAIL;
 
-        if (isSeller) {
+        if (!isSeller) {
             return res
                 .status(401)
                 .json({ success: false, message: "Nicht autorisiert" });
         }
 
         req.seller = {
-            email: tokenDecode.email,
-            isDemoSeller: tokenDecode.isDemoSeller
+            email: decoded.email,
+            isDemoSeller: decoded.isDemoSeller
         }
-
 next()
     } catch (error) {
         console.log(error.stack)
